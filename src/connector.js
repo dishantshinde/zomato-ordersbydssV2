9bf3bcf96385b59ce552cbf56ecdb13e29bb14cb
Dishant shinde <dishantshinde13@gmail.com>
2024-07-09 12:01:54 +0530

    edited connection limit

--- src/connector.js
+++ src/connector.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 15, // Adjust as per your application's needs
+  connectionLimit: 100, // Adjust as per your application's needs
   queueLimit: 0,
 });
 
